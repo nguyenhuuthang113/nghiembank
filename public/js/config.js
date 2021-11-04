1047d84b29ad3fc9ed3eafc8bf6b32709b2111d2
nguyenhuuthang113 <huuthang030603@gmail.com>
2021-11-04 19:31:39 +0700

    oik

---
+++ public/js/config.js
@@ -0,0 +1,31 @@
+const WEB = {
+    title: "THÔNG TIN THANH TOÁN",
+    favicon: "logoweb.png",
+};
+
+const USER = {
+    username: "Đăng Nghiêm",
+    blueTick: false,
+    avatar: "a.jpg",
+    skills: [
+        "DESINGER",
+        "DEVELOPER"
+    ],
+    banks: [
+        {
+            name: "VIETCOMBANK",
+            logo: "mbbank.png",
+            number: "1017420376",
+            owner: "Huỳnh Văn Minh Đăng Nghiêm"
+        },
+        
+        {
+            name: "MOMO",
+            logo: "momo.png",
+            number: "0979603297",
+            owner: "Huỳnh Văn Minh Đăng Nghiêm",
+            fastTransfer: true
+        },
+        
+    ]
+};
